1e9edae6c568c656e8aa55d9ba924e890f95abe4
Dominic Fox <dominic.fox@gmail.com>
2013-04-23 16:52:33 +0100

    Dominic: more tests on havePairsLike

--- src/test/scala/com/youdevise/matchless/HavePairsLikeSpec.scala
+++ src/test/scala/com/youdevise/matchless/HavePairsLikeSpec.scala
@@ -1,6 +1,7 @@
 package com.youdevise.matchless
 
 import Collections._
+import Matchers._
 import org.specs2.matcher.MustMatchers._
 import org.specs2.Specification
 
@@ -14,6 +15,20 @@ class HavePairsLikeSpec extends Specification {
         2 -> beEqualTo("two"),
         3 -> contain("ee")
       )
-  }
+    } ^
+    "fails to match a map missing any of the required keys" ! {
+      havePairsLike(
+        1 -> contain("ne"),
+        2 -> beEqualTo("two"),
+        3 -> contain("ee")
+      ) must failToMatchTheValue(Map(1 -> "one", 2 -> "two")).withMessageLike(contain("* 3: <missing value>"))
+    } ^
+    "fails to match a map with any non-matching values" ! {
+            havePairsLike(
+        1 -> contain("ne"),
+        2 -> beEqualTo("two"),
+        3 -> contain("ee")
+      ) must failToMatchTheValue(Map(1 -> "one", 2 -> "two", 3 -> "four")).withMessageLike(contain("* 3: 'four' doesn't contain 'ee'"))
+    }
 
 }
