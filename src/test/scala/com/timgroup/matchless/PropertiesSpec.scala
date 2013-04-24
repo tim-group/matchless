33157aca765b56e028a96c426ce0da7c8d55ea4e
Dominic Fox <dominic.fox@gmail.com>
2013-04-24 16:46:47 +0100

    Test hasProperties

--- src/test/scala/com/timgroup/matchless/PropertiesSpec.scala
+++ src/test/scala/com/timgroup/matchless/PropertiesSpec.scala
@@ -26,6 +26,20 @@ class PropertiesSpec extends Specification {
     "does not match if the property does not match" ! {
       haveProperty("foo", (v: TestClass) => v.foo).like(contain("giraffe")) must failToMatchTheValue(testValue)
         .withMessageLike(contain("The property <foo> of 'TestClass(My kingdom for a horse,42)' doesn't match the expectation"))
+    } ^ end ^
+  "The hasProperties matcher" ^
+    "matches if all properties are equal to the literal values" ! {
+      testValue must haveProperties(
+        ("foo", (v: TestClass) => v.foo, "My kingdom for a horse"),
+        ("bar", (v: TestClass) => v.bar, 42)
+      )
+    } ^
+    "does not match if any property is not equal to the literal value" ! {
+      haveProperties(
+        ("foo", (v: TestClass) => v.foo, "My dinkum for a shroe"),
+        ("bar", (v: TestClass) => v.bar, 43)
+      ) must failToMatchTheValue(testValue)
+        .withMessageLike(contain("My dinkum for a shroe") and contain("43"))
     } ^ end
     
 }
